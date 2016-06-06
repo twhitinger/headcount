6b13cd1ec8304a4435e5e14ac6b70f72275419ee
Trent Whitinger <twhitinger@gmail.com>
2016-06-06 17:20:10 -0600

    first pass through with methods passing

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -10,7 +10,11 @@ class Enrollment
     # floating point number representing a percentage for all years present in the dataset.
     enrollment_hash[:kindergarten_participation].each {|k, v|  enrollment_hash[:kindergarten_participation][k] = (v * 1000).floor / 1000.0 }
     enrollment_hash[:kindergarten_participation]
+  end
 
+  def kindergarten_participation_in_year(year)
+    binding.pry
+   (enrollment_hash[:kindergarten_participation][year] * 1000).floor / 1000.0
   end
 
 end
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -14,4 +14,9 @@ class EnrollmentTest < Minitest::Test
     assert_equal e.kindergarten_participation_by_year, {2010 => 0.391, 2011 => 0.353, 2012 => 0.267}
   end
 
+  def test_kidergarten_truncates_year_value
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal e.kindergarten_participation_in_year(2010), 0.391
+  end
 end
