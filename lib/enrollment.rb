f038438c53f3a3b716c21c5c5a586a05a7b3bdc5
Trent Whitinger <twhitinger@gmail.com>
2016-06-06 17:06:55 -0600

    things

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -5,11 +5,11 @@ class Enrollment
     @enrollment_hash = hash
   end
 
-  def kindergarten_participation_by_year(year)
+  def kindergarten_participation_by_year
     # This method returns a hash with years as keys and a truncated three-digit
     # floating point number representing a percentage for all years present in the dataset.
-    year[:kindergarten_participation].each {|k, v|  year[:kindergarten_participation][k] = (v * 1000).floor / 1000.0 }
-    year[:kindergarten_participation]
+    enrollment_hash[:kindergarten_participation].each {|k, v|  enrollment_hash[:kindergarten_participation][k] = (v * 1000).floor / 1000.0 }
+    enrollment_hash[:kindergarten_participation]
 
   end
 
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -11,7 +11,7 @@ class EnrollmentTest < Minitest::Test
   def test_kidergarten_truncates
     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
-    assert_equal e.kindergarten_participation_by_year(e.enrollment_hash), {2010 => 0.391, 2011 => 0.353, 2012 => 0.267}
+    assert_equal e.kindergarten_participation_by_year, {2010 => 0.391, 2011 => 0.353, 2012 => 0.267}
   end
 
 end
