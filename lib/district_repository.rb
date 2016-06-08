51b30d2947ec9d60bf782c0a3842babb3bdaaf09
Trent Whitinger <twhitinger@gmail.com>
2016-06-07 23:45:52 -0600

    updated the load method to avoid one huge run on, adjusted the pushing of the districts to work automatically, removed extra instance variables.

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -12,9 +12,7 @@ class DistrictRepository
   end
 
   def find_by_name(name)
-    @search = districts.find { |district| district.name == name.upcase }
-    insert_data_into_district if @search
-    @search
+    districts.find { |district| district.name == name.upcase }
   end
 
   def find_all_matching(fragment)
@@ -23,32 +21,33 @@ class DistrictRepository
     end
   end
 
-  def load_data(file_tree)
+  def load_data(repo_id)
+    create_district_repo(repo_id)
 
+    repo_id.each do |repo_type, files|
+      build_correct_repo(repo_type, files)
+    end
+    push_info_to_district
+  end
+
+  def create_district_repo(file_tree)
     filepath = file_tree[:enrollment][:kindergarten]
-
-    years =  CSV.foreach(filepath, headers: true, header_converters: :symbol).map do |row|
-      {name: row[:location], row[:timeframe].to_i => row[:data].to_f }
+    years =  CSV.foreach(filepath, headers: true, header_converters: :symbol).map(&:to_h)
+    years.each do |row|
+      next if find_by_name(row[:location].upcase)
+      districts << District.new(name: row[:location])
     end
-
-    per_district_by_year = years.group_by do |row|
-      row[:name]
-    end
-
-    district_data = per_district_by_year.map do |name, years|
-      merged = years.reduce({}, :merge)
-      merged.delete(:name)
-      { name: name, kindergarten_participation: merged }
-    end
-
-    district_data.each do |e|
-      @districts << District.new(e)
-    end
-    enrollment_repo = @enrollment_repo.load_data(file_tree)
   end
 
-  def insert_data_into_district
-    @search.enrollment = enrollment_repo.find_by_name(@search.name)
+  def build_correct_repo(repo_type, file_tree)
+    repositories = {enrollment: @enrollment_repo}
+    repo = repositories[repo_type]
+    repo.load_data({repo_type => file_tree})
   end
 
+  def push_info_to_district
+    districts.each do |district|
+      district.enrollment = @enrollment_repo.find_by_name(district.name)
+    end
+  end
 end
