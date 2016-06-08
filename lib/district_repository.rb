670a636b3b0eb6d453a950e7e0dc9d270888b510
Trent Whitinger <twhitinger@gmail.com>
2016-06-08 08:24:02 -0600

    updated to more descriptive name

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,6 +1,5 @@
 require "./lib/enrollment_repository"
 require "./lib/district"
-require "./lib/enrollment"
 require "csv"
 require "pp"
 
@@ -22,15 +21,15 @@ class DistrictRepository
   end
 
   def load_data(repo_id)
-    create_district_repo(repo_id)
+    generate_district_repo(repo_id)
 
     repo_id.each do |repo_type, files|
-      build_correct_repo(repo_type, files)
+      auto_generate_repo(repo_type, files)
     end
     push_info_to_district
   end
 
-  def create_district_repo(file_tree)
+  def generate_district_repo(file_tree)
     filepath = file_tree[:enrollment][:kindergarten]
     years =  CSV.foreach(filepath, headers: true, header_converters: :symbol).map(&:to_h)
     years.each do |row|
@@ -39,7 +38,7 @@ class DistrictRepository
     end
   end
 
-  def build_correct_repo(repo_type, file_tree)
+  def auto_generate_repo(repo_type, file_tree)
     repositories = {enrollment: @enrollment_repo}
     repo = repositories[repo_type]
     repo.load_data({repo_type => file_tree})
