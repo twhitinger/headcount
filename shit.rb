fc6c5b10cf39792b60715adc48e612561abdbf17
Zack Forbing <zackforbing@gmail.com>
2016-06-13 11:31:46 -0600

    getting there with parser

--- shit.rb
+++ shit.rb
@@ -2,14 +2,16 @@ require "csv"
 require "pry"
 
 years = CSV.readlines("/Users/zackforbing/turing/1module/headcount/data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv", headers: true, header_converters: :symbol).map do |row|
-{name: row[:location], row[:timeframe].to_i => {row[:score].downcase.to_sym => row[:data].to_f}}
+{name: row[:location], timeframe: row[:timeframe].to_i, row[:score].downcase.to_sym => row[:data].to_f} #.reduce({}, :merge)
 end
 scores_by_school = years.group_by do |row|
   row[:name]
 end
-all_scores = scores_by_school.map do |name, years|
-  merged = years.reduce({}, :merge)
+
+all_scores = scores_by_school.map do |name, year|
+  merged = year.reduce({}, :merge)
   merged.delete(:name)
   merged
 end
+#create a hash based on timeframe, 
 puts all_scores
