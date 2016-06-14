9d3008049bb9eadbd527a67a76a399d17b584341
Trent Whitinger <twhitinger@gmail.com>
2016-06-14 10:29:41 -0600

    THE PARSER WILL DO ALL THE FILES FORMATTED CORRECTLY

--- lib/statewide_test_repository.rb
+++ lib/statewide_test_repository.rb
@@ -19,57 +19,56 @@ class StatewideTestRepository
       shit_together = scores_by_location.each_with_object({}) do |(name, district_data), subject_data|
         single_district_data(name, district_data, subject_data)
       end
-
       shit_together.each do |location_name, data|
+
         if find_by_name(location_name)
-          #do some shit to merge the hash to the current
+          find_by_name(location_name).class_data[source] = data
         else
-        @statewide_tests[location_name] = StatewideTest.new({source => data})
+          @statewide_tests[location_name] = StatewideTest.new({source => data})
+        end
       end
-
-
-
-    #   if source == :third_grade
-    #     @store_all_files[shit_together]
-    #   elsif source == :eighth_grade
-    #     @store_all_files[8] = shit_together
-    #   elsif source == :math
-    #     @store_all_files[:math] = shit_together
-    #   elsif source == :reading
-    #     @store_all_files[:reading] = shit_together
-    #   else
-    #     @store_all_files[:writing] = shit_together
-    #   end
-    # end
-
-    # @statewide_tests = StatewideTest.new(@store_all_files)
+    end
   end
 
   def find_by_name(district_name)
     statewide_tests[district_name]
     # returns either nil or an instance of StatewideTest having done a case insensitive search
+
   end
 
   def single_subject_data(year, data, district_data)
+
+
     one_subject_data = data.each_with_object({}) do |row, subject_data|
-      subject_data[row[:score].downcase.to_sym] = sanitize_data_to_na(row[:data])
+
+      subject_data[row[class_or_race(row)].downcase.to_sym] = sanitize_data_to_na(row[:data])
       # subject_data[row[:timeframe].to_i] = sanitize_data_to_na(row[:data])
     end
     district_data[year] = one_subject_data
   end
 
   def single_district_data(name, district_data, subject_data)
+
     grouped_data = group_by_year(district_data)
     subject_data[name] = grouped_data.each_with_object({}) do |(year, data), district_data|
+
       single_subject_data(year, data, district_data)
     end
   end
 
   def group_by_year(data)
+
+    # [class_or_race(row)]
+    # row[class_or_race(row)]
     data.group_by { |row| row[:timeframe].to_i }
   end
+  # def group_by_subject(data)
+  #
+  #   data.group_by { |row| row[class_or_race(row)] }
+  # end
 
   def class_or_race(row)
+
     if row.has_key?(:score)
       :score
     elsif row.has_key?(:race_ethnicity)
@@ -78,13 +77,15 @@ class StatewideTestRepository
   end
 
   def sanitize_data(input)
+
     input.to_s.gsub!(/[\s]+/,'')
     input = input.to_f if String === input
     input
   end
 
   def sanitize_data_to_na(num)
-    if sanitize_data(num) == 0 || sanitize_data(num).to_s.upcase.chars[0] == "N"
+
+    if sanitize_data(num) == 0 || sanitize_data(num).to_s.chars[0] == "N"
       "N/A"
     else
       sanitize_data(num)
@@ -92,3 +93,16 @@ class StatewideTestRepository
   end
 
 end
+
+
+# if source == :third_grade
+#   @store_all_files[shit_together]
+# elsif source == :eighth_grade
+#   @store_all_files[8] = shit_together
+# elsif source == :math
+#   @store_all_files[:math] = shit_together
+# elsif source == :reading
+#   @store_all_files[:reading] = shit_together
+# else
+#   @store_all_files[:writing] = shit_together
+# end
