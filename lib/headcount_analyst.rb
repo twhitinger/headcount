c4e61a992da3f80d765876c41f58c340f91a208a
Trent Whitinger <twhitinger@gmail.com>
2016-06-15 18:44:23 -0600

    iteration 6 going places

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,4 +1,6 @@
 require_relative 'district_repository'
+require_relative 'result_set'
+require_relative 'result_entry'
 require_relative 'math_helper'
 class HeadcountAnalyst
 
@@ -7,7 +9,7 @@ class HeadcountAnalyst
     @statewide = []
     @passed_average_filter = []
     @passed_lunch_filter = []
-    @avg_hash
+    @passed_HS_filter = []
   end
 
   def kindergarten_participation_rate_variation(location_one, location_two)
@@ -95,64 +97,115 @@ class HeadcountAnalyst
       (occurances.to_f / total) > 0.7
     end
 
-
     def high_poverty_and_high_school_graduation
-      average_for_each_district_qualify_for_free_lunch
       average_for_each_district_poverty
-
-    end
-
-    def average_for_each_district_poverty
-      total = @district_repo.districts.find_all do |school|
-        school.economic_profile.economic_data[:children_in_poverty]
-      end
-      total.map do |school|
-        average = school.economic_profile.economic_data[:children_in_poverty].values.reduce(:+)/
-        school.economic_profile.economic_data[:children_in_poverty].length
-        is_it = average > average_for_all_districts_poverty
-
-        # if the district is greater then the average poverty
-        # pass it to the next method qualify for free lunch method
-
-        @passed_average_filter << school if is_it
-      end
-    end
-
-    def average_for_all_districts_poverty
-      total = @district_repo.districts.find_all do |school|
-        school.economic_profile.economic_data[:children_in_poverty]
-      end
-      values = total.reduce([]) do |result, school|
-        all_values = school.economic_profile.economic_data[:children_in_poverty].values
-        result << all_values
-        result
-      end.flatten
-      average = values.reduce(:+)/values.count
-    end
-
-    def average_for_each_district_qualify_for_free_lunch
-      total = @district_repo.districts.find_all do |school|
-        school.economic_profile.economic_data[:children_in_poverty]
-      end
-      total.map do |school|
-        average = school.economic_profile.economic_data[:free_or_reduced_price_lunch]
-        each_avg = average.reduce([]) do |result, data|
-          result << data.last[:total]
+      matching_districts = []
+      @passed_HS_filter.reduce([]) do |result, school|
+        passing_districts = ResultEntry.new({name: school.name, free_and_reduced_price_lunch_rate:
+          district_average(school), children_in_poverty_rate: poverty_dist_average(school),
+          high_school_graduation_rate: hs_district_average(school)})
+          matching_districts << passing_districts
         end
-        final_avg = each_avg.reduce(:+)/each_avg.length
-        is_it = final_avg > average_for_all_districts_qualify_for_free_lunch
-        @passed_lunch_filter << school if is_it
+        statewide_result = ResultEntry.new({name: "COLORADO", free_and_reduced_price_lunch_rate:
+          average_for_all_districts_qualify_for_free_lunch, children_in_poverty_rate: average_for_all_districts_poverty,
+          high_school_graduation_rate: average_for_all_districts_high_school_graduation})
+          high_poverty_and_grad_result = ResultSet.new(matching_districts: matching_districts,statewide_average: statewide_result)
+        end
+
+        def poverty_dist_average(school)
+          average = school.economic_profile.economic_data[:children_in_poverty].values.reduce(:+)/
+          school.economic_profile.economic_data[:children_in_poverty].length
+        end
+
+        def district_average(school)
+          average = school.economic_profile.economic_data[:free_or_reduced_price_lunch]
+          each_avg = average.reduce([]) do |result, data|
+            result << data.last[:total]
+          end
+          final_avg = each_avg.reduce(:+)/each_avg.length
+        end
+
+        def hs_district_average(school)
+          path = school.enrollment.high_school_data[:high_school_graduation_participation].values
+          total = path.reduce(:+)
+          length = path.length
+          total/length
+        end
+
+
+        def average_for_each_district_poverty
+          total = @district_repo.districts.find_all do |school|
+            school.economic_profile.economic_data[:children_in_poverty]
+          end
+          total.map do |school|
+            average = school.economic_profile.economic_data[:children_in_poverty].values.reduce(:+)/
+            school.economic_profile.economic_data[:children_in_poverty].length
+            is_it = average > average_for_all_districts_poverty
+
+            # if the district is greater then the average poverty
+            # pass it to the next method qualify for free lunch method
+            @passed_average_filter << school if is_it
+          end
+          average_for_each_district_qualify_for_free_lunch(@passed_average_filter)
+        end
+
+        def average_for_all_districts_poverty
+          total = @district_repo.districts.find_all do |school|
+            school.economic_profile.economic_data[:children_in_poverty]
+          end
+          values = total.reduce([]) do |result, school|
+            all_values = school.economic_profile.economic_data[:children_in_poverty].values
+            result << all_values
+            result
+          end.flatten
+          average = values.reduce(:+)/values.count
+        end
+
+        def average_for_each_district_qualify_for_free_lunch(schools)
+
+          # @passed_average_filter[0]
+          total = schools.find_all do |school|
+            school.economic_profile.economic_data[:free_or_reduced_price_lunch]
+          end
+          total.map do |school|
+            average = school.economic_profile.economic_data[:free_or_reduced_price_lunch]
+            each_avg = average.reduce([]) do |result, data|
+              result << data.last[:total]
+            end
+            final_avg = each_avg.reduce(:+)/each_avg.length
+            is_it = final_avg > average_for_all_districts_qualify_for_free_lunch
+            @passed_lunch_filter << school if is_it
+          end
+          average_for_each_district_high_school_graduation(@passed_lunch_filter)
+        end
+
+        def average_for_all_districts_qualify_for_free_lunch
+          total = @district_repo.districts[0].economic_profile.economic_data[:free_or_reduced_price_lunch].values
+          values = total.reduce([]) do |result, data|
+            result << data[:total]
+            result
+          end.flatten
+          average = values.reduce(:+)/values.count/@district_repo.districts.length
+          # Above the statewide average in number of students qualifying for free and reduced price lunch
+        end
+
+
+        def average_for_each_district_high_school_graduation(schools)
+          total = schools.find_all do |school|
+            school.enrollment.high_school_data[:high_school_graduation_participation]
+          end
+          total.map do |school|
+            each_avg = school.enrollment.high_school_data[:high_school_graduation_participation].values
+            final_avg = each_avg.reduce(:+)/each_avg.length
+            is_it = final_avg > average_for_all_districts_high_school_graduation
+            @passed_HS_filter << school if is_it
+          end
+        end
+
+        def average_for_all_districts_high_school_graduation
+          path = @district_repo.districts[0].enrollment.high_school_data[:high_school_graduation_participation].values
+          total = path.reduce(:+)
+          length = path.length
+          total/length
+        end
       end
-    end
-
-    def average_for_all_districts_qualify_for_free_lunch
-      total = @district_repo.districts[0].economic_profile.economic_data[:free_or_reduced_price_lunch].values
-      values = total.reduce([]) do |result, data|
-        result << data[:total]
-        result
-      end.flatten
-      average = values.reduce(:+)/values.count/@district_repo.districts.length
-      # Above the statewide average in number of students qualifying for free and reduced price lunch
-    end
-
-  end
